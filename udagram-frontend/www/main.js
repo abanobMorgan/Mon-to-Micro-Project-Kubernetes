bd4da77f84079fbe6ff31acf07493e65c91d1c11
abanob.k.morgan <abanob.k.morgan@gmail.com>
2023-04-05 00:28:36 +0200

    Fix in the deployment

--- udagram-frontend/www/main.js
+++ udagram-frontend/www/main.js
@@ -1660,7 +1660,7 @@ __webpack_require__.r(__webpack_exports__);
 var environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: 'http://ab966d56941da484a86480f1ce4dfdd6-685768176.us-east-1.elb.amazonaws.com:8080/api/v0'
+    apiHost: 'http://localhost/:8080/api/v0'
 };
 /*
  * For easier debugging in development mode, you can import the following file
