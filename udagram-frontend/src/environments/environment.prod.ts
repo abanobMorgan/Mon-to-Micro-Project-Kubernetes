ea4e1415e18de4334b500027c5cdc7f865312cfd
abanob.k.morgan <abanob.k.morgan@gmail.com>
2023-04-08 20:00:53 +0200

    fix bug

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a10beb556c65e42f1a3234db940b7e83-106431253.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a10beb556c65e42f1a3234db940b7e83-106431253.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
+ *  
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
